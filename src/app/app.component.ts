f231e1c63d1352ff66320f5ab9007721f715e6d5
epation <ionut.i.patrascu@ericsson.com>
2017-06-09 17:49:30 +0300

    img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
+import { MenuService } from '../fw/services/menu.service';
+import { initialMenuItems } from './app.menu';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  constructor(private frameworkConfigService: FrameworkConfigService,
+              private menuService: MenuService){
+    let config:FrameworkConfigSettings = {
+      socialIcons: [
+        { imageFile: 'assets/social-fb-bw.png', alt: 'Facebook', link: 'http://www.facebook.com' },
+        { imageFile: 'assets/social-google-bw.png', alt: 'Google +', link: 'https://plus.google.com' },
+        { imageFile: 'assets/social-twitter-bw.png', alt: 'Twitter', link: 'http://twitter.com' }
+      ],
+      showLanguageSelector : true,
+      showUserControls : true,
+      showStatusBar : true,
+      showStatusBarBreakpoint : 800
+    }
+    menuService.items = initialMenuItems;
+    frameworkConfigService.configure(config);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,41 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FwModule } from '../fw/fw.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { RouterModule} from '@angular/router';
+import { appRoutes } from './app.routing';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryMaintComponent } from './country-maint/country-maint.component';
+import { AuthenticatedUserComponent } from './authenticated-user/authenticated-user.component';
+import { UserService } from './services/user.service';
+import { UserApi } from '../fw/users/user-api';
+import { AuthGuard } from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SettingsComponent,
+    CountryDetailComponent,
+    CountryListComponent,
+    CountryMaintComponent,
+    AuthenticatedUserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FwModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    UserService,
+    AuthGuard,
+    { provide: UserApi, useExisting: UserService }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
